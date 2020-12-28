dcab4820237a8f18a30ce2bbec4e744464660924
Darko Krspogacin <darko.krspogacin@vivifyideas.com>
2020-12-28 12:52:57 +0100

    add movie pagination

--- imdb-frontend-app/src/components/MovieList.vue
+++ imdb-frontend-app/src/components/MovieList.vue
@@ -5,27 +5,32 @@
       :key="movie.id"
       :movie="movie"/>
   </div>
+  <MovieListPagination :data="paginationData" @changeData="changeData"/>
 </template>
 
 <script>
 import MovieListItem from './MovieListItem.vue'
+import MovieListPagination from './MovieListPagination.vue'
 import axios from 'axios'
 
 export default {
   name: 'MovieList',
   components: {
-    MovieListItem
+    MovieListItem,
+    MovieListPagination
   },
   data() {
     return {
       movieList: [],
+      paginationData: [],
     }
   },
   created() {
     axios.get('movies')
       .then(
         response => {
-          this.movieList = response.data;
+          this.movieList = response.data.data;
+          this.paginationData = response.data;
         }
       ).catch(
         error => {
@@ -33,5 +38,12 @@ export default {
         }
       )
   },
+  methods: {
+    changeData(data) {
+      this.movieList = data.data;
+      this.paginationData = data;
+      window.scrollTo(0, 0);
+    }
+  }
 }
 </script>
---
+++ imdb-frontend-app/src/components/MovieListPagination.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav aria-label="Page navigation example">
+    <ul class="pagination justify-content-center">
+      <div  v-for="link in data.links" :key="link.label">
+        <li class="page-item" :class="{ disabled: !link.url || link.active }">
+          <a class="page-link" :class="{ 'text-success':  link.url && !link.active, 'bg-success': link.active }" href="javascript:void(0)" @click="handleClick(link.url)"><span v-html="link.label"></span></a>
+        </li>
+      </div>
+      <p class="ml-3 mt-2"> Showing {{ data.from }} to {{ data.to }} of {{ data.total }} movies
+    </ul>
+  </nav>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'MovieListPagination',
+  props: ['data'],
+  methods: {
+    handleClick(url) {
+      const index = url.indexOf("movies");
+      const relativeUrl = url.slice(index, url.length);
+      
+      axios.get(relativeUrl)
+      .then(
+        response => {
+          this.$emit('changeData', response.data);
+        }
+      ).catch(
+        error => {
+          alert('Server error, try again');
+        }
+      )
+    }
+  }
+}
+</script>
+
+<style scoped>
+li:disabled {
+  color: text-secondary;
+}
+</style>
