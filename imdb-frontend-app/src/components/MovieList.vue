93fd411d8bc48e19ff15e41c9b8edc960eba4c64
Darko Krspogacin <darko.krspogacin@vivifyideas.com>
2020-12-29 11:54:29 +0100

    add movies search

--- imdb-frontend-app/src/components/MovieList.vue
+++ imdb-frontend-app/src/components/MovieList.vue
@@ -1,4 +1,8 @@
 <template>
+  <div class="form-group form-inline">
+    <label for="search" class="h5 ml-auto mr-3">Search (movie title): </label>
+    <input id="search" v-debounce="searchMovies" type="text" class="form-control form-control-lg w-25 mr-auto" placeholder="Title">
+  </div>
   <div class="card-deck">
     <MovieListItem
       v-for="movie in movieList"
@@ -33,5 +37,37 @@ export default {
         }
       )
   },
+  methods: {
+    searchMovies(value, e) {
+      if (value === ''){
+        axios.get('movies')
+          .then(
+            response => {
+              this.movieList = response.data;
+            }
+          ).catch(
+            error => {
+              alert('Server error, try again');
+            }
+        )
+        return;
+      }
+      
+      const data = {
+        title: value,
+      }
+
+      axios.get('movies/search', { params: data })
+      .then(
+        response => {
+          this.movieList = response.data.data;
+        }
+      ).catch(
+        error => {
+          alert('Server error, try again');
+        }
+      )
+    }
+  }
 }
 </script>
