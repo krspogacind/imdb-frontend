ce0b744f46d22d54d8041ed857367ce6a88d437a
Darko Krspogacin <darko.krspogacin@vivifyideas.com>
2020-12-31 16:39:30 +0100

    add movie comments

--- imdb-frontend-app/src/components/MovieListItemPage.vue
+++ imdb-frontend-app/src/components/MovieListItemPage.vue
@@ -9,9 +9,9 @@
     </div>
     <img class="img-fluid rounded mw-100" style="height: auto" :src="movie.cover_image_url" alt="Movie image">
     <div class="border border-secondary rounded">
-      <h4 class="text-secondary">Movie description: </h4>
+      <h4 class="text-secondary mt-3">Movie description: </h4>
       <p class="text-secondary"> {{ movie.description }} </p>
-      <div class="text-right text-secondary mr-3">
+      <div class="text-right text-secondary mr-3 mb-3">
         <a href="javascript:void(0)" @click="like(true)">
           <img v-if="!isLiked"  id="like" class="icon" src="../assets/like.png" /> 
           <img v-else id="like" class="icon" src="../assets/like_done.png" /> 
@@ -22,6 +22,24 @@
           <img v-else id="dislike" class="icon ml-3" src="../assets/dislike_done.png" />
         </a>
         {{ dislikes }}
+      </div>
+    </div>
+    <div class="border border-secondary rounded justify-content-center mb-4">
+      <h4 class="text-secondary mt-3">Movie comments: </h4>
+      <button v-if="user && !showTextArea" class="btn btn-warning mb-4" @click="showTextArea = true"> Add comment </button>
+      <button v-if="showTextArea" class="btn btn-success mb-4" @click="addComent"> Submit comment </button>
+      <button v-if="showTextArea" class="btn btn-secondary ml-4 mb-4" @click="showTextArea = false"> Cancel </button>
+      <br>
+      <small>
+        {{  errorMessage  }}
+      </small>
+      <textarea @input="deleteMessage" v-if="showTextArea" class="form-control ml-4 mb-4 comment" v-model="commentContent" placeholder="Comment" rows="3"></textarea>
+      <div v-for="comment in comments" :key="comment.id" class="ml-4 mb-4">
+        <div class="border border-secondary rounded comment">
+          <b> {{ comment.user.name }} </b>
+          <div> {{ comment.content }} </div>
+        </div>
+        <small class="text-secondary float-left ml-3"> {{ comment.created_at }} </small>
       </div>
     </div>
   </div>
@@ -41,6 +59,10 @@ export default {
       dislikes: 0,
       isLiked: false,
       isDisliked: false,
+      showTextArea: false,
+      commentContent: '',
+      errorMessage: '',
+      comments: [],
     }
   },
   computed: {
@@ -60,6 +82,27 @@ export default {
           if (error.response.status === 404){
             this.$wkToast(error.response.data.error);
             this.$router.push('/movies');
+          } else {
+            alert('Server error, try again');
+          }
+        }
+      )
+
+    axios.get('movies/comment/' + this.$route.params.id)
+      .then(
+        response => {
+          response.data.forEach(element => {
+            const index = element.created_at.indexOf('.');
+            element.created_at = element.created_at.slice(0, index).replace('T', ' ');
+
+          });
+          this.comments = response.data;
+        }
+      ).catch(
+        error => {
+          if (error.response.status === 401){
+            this.$wkToast('Need to login to see movie comments');
+            this.$router.push('/login');
           } else {
             alert('Server error, try again');
           }
@@ -118,6 +161,49 @@ export default {
         }
       )
 
+    },
+
+    addComent() {
+      if (this.commentContent === '') {
+        this.errorMessage = "Comment is empty"
+        return;
+      }
+      if (this.commentContent.length > 500) {
+        this.errorMessage = "Comment is longer than 500 characters"
+        return;
+      }
+
+      const data = {
+        movie_id: this.movie.id,
+        content: this.commentContent,
+      }
+
+      this.showTextArea = false;
+
+      axios.post('movies/comment', data)
+      .then(
+        response => {
+          const comment = response.data.comment[0];
+          const index = comment.created_at.indexOf('.');
+          comment.created_at = comment.created_at.slice(0, index).replace('T', ' ');
+          this.comments.unshift(comment);
+          this.$wkToast(response.data.message);
+        }
+      ).catch(
+        error => {
+          if (error.response.status === 401){
+            this.$wkToast('Need to login to comment movies');
+          } else {
+            alert('Server error, try again');
+          }
+        }
+      )
+    },
+
+    deleteMessage() {
+      if (this.errorMessage !== null){
+        this.errorMessage = null;
+      }
     }
   }
 }
@@ -131,5 +217,13 @@ a {
 .icon {
   width: 30px;
   height: 30px;
+}
+
+.comment {
+  width: 95%;
+}
+
+small {
+  color: red;
 }
 </style>
